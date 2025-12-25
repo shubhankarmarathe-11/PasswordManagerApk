ee866026b4df86f414c33b7f482dec7bd55e097f
= <=>
2025-12-25 14:40:10 +0530

    nativewind setup

---
+++ app/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-pink">
+      <Text className="text-xl font-bold text-blue-200">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
