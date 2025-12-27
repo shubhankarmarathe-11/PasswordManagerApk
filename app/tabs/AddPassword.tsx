d68d3c30abfe9c86e29501def7e4eb9088430c74
= <=>
2025-12-27 22:52:15 +0530

    create base frontend for home and AddPassword screen

--- app/tabs/AddPassword.tsx
+++ app/tabs/AddPassword.tsx
@@ -1,10 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 
 const AddPassword = () => {
   return (
     <>
       <View className=" flex-1 pt-24 w-screen ">
-        <Text>AddPassword</Text>
+        <View className="p-3 gap-5">
+          <Text className="text-center text-3xl ">Add Password</Text>
+          <Text className="font-bold">Website / App name</Text>
+          <TextInput
+            className="border border-1 rounded p-3 text-lg font-extralight"
+            placeholder="Enter Website / App name"
+          />
+          <Text className="font-bold">Username / email</Text>
+          <TextInput
+            className="border border-1 rounded p-3 text-lg font-extralight"
+            placeholder="Enter username / email"
+          />
+          <Text className="font-bold">Password</Text>
+          <TextInput
+            className="border border-1 rounded p-3 text-lg font-extralight"
+            placeholder="Enter Password"
+          />
+
+          <Text
+            // onPress={}
+            className="w-full bg-violet-700 text-center text-white rounded-md p-5 font-bold "
+          >
+            Submit
+          </Text>
+        </View>
       </View>
     </>
   );
--- app/tabs/Homepage.tsx
+++ app/tabs/Homepage.tsx
@@ -1,10 +1,52 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
 
 const Homepage = () => {
+  const [totalpass, Settotalpass] = useState(2);
+  const [user, Setuser] = useState("Shubhankar Marathe");
+
   return (
     <>
       <View className=" flex-1 pt-24 w-screen ">
-        <Text>Home</Text>
+        <View className="p-3 gap-5">
+          <Text className="text-center text-2xl font-bold break-words">
+            👋 Welcome {user}
+          </Text>
+          {/* Top view with two view left and right */}
+          <View className="flex flex-row justify-start items-center">
+            <View className="bg-violet-200 h-48 w-48 rounded-md flex items-center justify-center">
+              <Text className="text-center text-black">Passwords Saved</Text>
+              <Text className="text-center text-3xl text-black">
+                {totalpass}
+              </Text>
+            </View>
+          </View>
+          <Pressable className="bg-violet-700 rounded-md py-16 flex justify-center items-center">
+            <Text className="text-white text-lg">Go to Saved Passwords</Text>
+          </Pressable>
+          <Text className="text-lg font-bold">
+            Best Practices to Create strong Password
+          </Text>
+          <Text className="mb-1">• Use long passwords (12–16 characters)</Text>
+          <Text className=" mb-1">
+            • Combine uppercase, lowercase, numbers, and symbols
+          </Text>
+          <Text className=" mb-1">
+            • Avoid personal information and common words
+          </Text>
+          <Text className=" mb-1">
+            • Use a unique password for every account
+          </Text>
+          <Text className=" mb-1">
+            • Change passwords immediately if compromised
+          </Text>
+          <Text className=" mb-1">
+            • Enable Two-Factor Authentication (2FA)
+          </Text>
+          <Text className="">
+            • Use a password manager to generate and store passwords securely
+          </Text>
+        </View>
       </View>
     </>
   );
